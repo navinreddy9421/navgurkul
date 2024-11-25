0f8a154c4f2bbe79cfdae584e03e883f9e0d226b
puranchandralohar <clpuran95@gmail.com>
2024-11-25 17:32:22 +0530

    fix: campus as custom and replace setup with navgurukul module

---
+++ navgurukul/navgurukul/doctype/campus/campus.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Navgurukul and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Campus", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ navgurukul/navgurukul/doctype/campus/test_campus.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Navgurukul and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCampus(FrappeTestCase):
+	pass
